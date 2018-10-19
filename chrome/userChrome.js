cefbd3c9800f47406a4dd87aa0b3b34381e02c51
Vadim Khitrin <me@vkhitrin.com>
2018-10-19 19:46:22 +0300

    Tweaking context menu, tooltip and reload icon

--- chrome/userChrome.js
+++ chrome/userChrome.js
@@ -1,11 +1,22 @@
 statuspanel = document.getElementById("statuspanel");
+stopreloadbutton = document.getElementById("stop-reload-button");
 var mutationObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         statuspanel.removeAttribute("mirror");
+        stopreloadbutton.removeAttribute("animate");
     });
   });
 
 mutationObserver.observe(statuspanel, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+
+mutationObserver.observe(stopreloadbutton, {
   attributes: true,
   characterData: true,
   childList: true,
